d8b89df1a48311db84e903634dcc506d297437ba
andriikuklinov <akuklinov@outlook.com>
2025-04-27 00:41:21 +0300

    Define IGenericRepository interface.

---
+++ AutoPartsStore/Common/Data.Common/Contract/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Common.Contract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
+        IQueryable<T> Get();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> UpdateRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> DeleteRangeAsync(IEnumerable<T> entities);
+    }
+}
