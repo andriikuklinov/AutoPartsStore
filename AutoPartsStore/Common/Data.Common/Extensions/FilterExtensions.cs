f56cf039226b565165ba88148f6126c03a530737
andriikuklinov <akuklinov@outlook.com>
2025-04-30 13:54:54 +0300

    Filter extension method tests.

--- AutoPartsStore/Common/Data.Common/Extensions/FilterExtensions.cs
+++ AutoPartsStore/Common/Data.Common/Extensions/FilterExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Data.Common.Models;
+using System.Globalization;
 using System.Linq.Expressions;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -9,24 +10,35 @@ namespace Data.Common.Extensions
     {
         public static IQueryable<T> Filter<T>(this IQueryable<T> query, string filter) where T : class
         {
-            if(filter != null || filter.ToString() != string.Empty)
+            if (filter != null || filter.ToString() != string.Empty)
             {
-                var filterData = JsonSerializer.Deserialize<FilterData<string>>(filter, new JsonSerializerOptions()
+                var filterData=new FilterData<string>();
+                try
                 {
-                    PropertyNameCaseInsensitive=true,
-                    NumberHandling=JsonNumberHandling.AllowReadingFromString
-                });
+                    filterData = JsonSerializer.Deserialize<FilterData<string>>(filter, new JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true,
+                        NumberHandling = JsonNumberHandling.AllowReadingFromString
+                    });
+                    if(filterData.Data==null)
+                        throw new ArgumentException("Parameter filter is invalid (contain wrong json).", nameof(filter));
+                }
+                catch (JsonException ex)
+                {
+                    throw new ArgumentException("Parameter filter is invalid (contain wrong json).", nameof(filter), ex);
+                }
                 var parameter = Expression.Parameter(typeof(T), "_");
 
                 Expression filterExpression = null;
-                foreach(var filterModel in filterData.Data)
+                foreach (var filterModel in filterData.Data)
                 {
-                    var property = Expression.Property(parameter, filterModel.PropertyName);
-                    var constant = Expression.Constant(filterModel.Value, typeof(string));
+                    var property = Expression.Property(parameter, typeof(T), filterModel.PropertyName);
+                    Type searchValueType = DetectSearchValueType(filterModel.Value);
+                    var constant = Expression.Constant(Convert.ChangeType(filterModel.Value, searchValueType), searchValueType);
                     Expression comparison = null;
-                    if(property.Type == typeof(string))
+                    if (property.Type == typeof(string))
                     {
-                        comparison=Expression.Call(property, "Contains", Type.EmptyTypes, constant);
+                        comparison = Expression.Call(property, "Contains", Type.EmptyTypes, constant);
                     }
                     else
                     {
@@ -39,6 +51,17 @@ namespace Data.Common.Extensions
                 return query.Where(lambda);
             }
             return query;
+        }
+
+        private static Type DetectSearchValueType(string value)
+        {
+            if (int.TryParse(value, CultureInfo.InvariantCulture, out _))
+                return typeof(int);
+            if (decimal.TryParse(value, CultureInfo.InvariantCulture, out _))
+                return typeof(decimal);
+            if (DateTime.TryParse(value, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+                return typeof(DateTime);
+            return typeof(string);
         }
     }
 }
---
+++ AutoPartsStore/Tests/Data.Common.Tests/FilterExtensionsTests.cs
@@ -0,0 +1,66 @@
+using Data.Common.Extensions;
+using Data.Common.Tests.Models;
+using System.Diagnostics;
+
+namespace Data.Common.Tests
+{
+    [TestFixture]
+    public class FilterExtensionsTests
+    {
+        private IEnumerable<Product> _products;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _products = new List<Product>
+            {
+                new Product{ Id = 1, Name = "Accumulator", Price = 123.5M },
+                new Product{ Id = 1, Name = "Accumulator 2", Price = 127.2M },
+                new Product{ Id = 1, Name = "Accumulator_M2BS", Price = 103.15M },
+                new Product{ Id = 1, Name = "Tire 1", Price = 211.25M },
+                new Product{ Id = 1, Name = "Tire 1", Price = 123 },
+                new Product{ Id = 1, Name = "Tire ", Price = 78.89M },
+            };
+        }
+
+        [Test]
+        public void Filter_WhenCallWithFilterByName_ReturnsFilteredCollectionWithThreeElements()
+        {
+            //Arrange
+            var filter = "{\"data\":[{\"PropertyName\":\"Name\",\"Value\":\"Acc\"}]}";
+            //Act
+            var result = _products.AsQueryable<Product>().Filter<Product>(filter).ToList<Product>();
+            //Assert
+            Assert.That(result.Count(), Is.EqualTo(3));
+        }
+        [Test]
+        public void Filter_WhenCallWithFilterByPrice_ReturnsCollectionWithOneElement()
+        {
+            //Arrange
+            var filter = "{\"data\":[{\"PropertyName\":\"Price\",\"Value\":\"211,25\"}]}";
+            //Act
+            var result = _products.AsQueryable<Product>().Filter<Product>(filter).ToList<Product>();
+            //Assert
+            Assert.That(result.Count(), Is.EqualTo(1));
+        }
+        [Test]
+        public void Filter_WhenCallWithNonExistingValue_ReturnsEmptyCollection()
+        {
+            //Arrange
+            var filter = "{\"data\":[{\"PropertyName\":\"Name\",\"Value\":\"FFFGSX\"}]}";
+            //Act
+            var result = _products.AsQueryable<Product>().Filter<Product>(filter).ToList<Product>();
+            //Assert
+            Assert.That(result.Count(), Is.EqualTo(0));
+        }
+        [Test]
+        [TestCase("{[]!}")]
+        [TestCase("{}")]
+        [TestCase("")]
+        [TestCase("[]")]
+        public void Filter_WhenCallWithInvalidFilter_ThrowAnArgumentException(string filter)
+        {
+            Assert.That(() => _products.AsQueryable<Product>().Filter<Product>(filter).ToList<Product>(), Throws.ArgumentException);
+        }
+    }
+}
