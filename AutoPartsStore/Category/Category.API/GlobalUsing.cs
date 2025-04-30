b3c7320b1445d29d8e2a73eae0dd09c820514339
andriikuklinov <akuklinov@outlook.com>
2025-04-30 20:43:25 +0300

    Create global usings file.

---
+++ AutoPartsStore/Category/Category.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using MediatR;
+global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using Common.CQRS;
+global using Category.API.Data.Repositories.Contracts;
+global using Category.API.Categories.Queries.GetCategories;
+global using Category.API.Models.GetCategories;
