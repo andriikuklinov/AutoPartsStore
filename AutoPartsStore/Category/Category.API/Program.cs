31d28128b382ad78a25ce29d290466fd53996528
andriikuklinov <akuklinov@outlook.com>
2025-04-30 23:16:01 +0300

    Add Microsoft.EntityFrameworkCore.SqlServer package to Data.Common assembly.

--- AutoPartsStore/Category/Category.API/Program.cs
+++ AutoPartsStore/Category/Category.API/Program.cs
@@ -1,9 +1,37 @@
+using Category.API.Data;
+using Category.API.Data.Repositories;
+using Category.API.MappingProfiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+#region Route options
+builder.Services.Configure<RouteOptions>(config =>
+{
+    config.LowercaseUrls = true;
+    config.LowercaseQueryStrings = true;
+});
+#endregion
+#region Dependencies
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+#endregion
+#region Automapper
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new CategoryMappingProfile());
+});
+#endregion
+#region DbContext
+builder.Services.AddDbContext<CategoryContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CategoryDatabase"), optionBuilder =>
+    {
+        optionBuilder.MigrationsAssembly("Category.API");
+    });
+});
+#endregion
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
