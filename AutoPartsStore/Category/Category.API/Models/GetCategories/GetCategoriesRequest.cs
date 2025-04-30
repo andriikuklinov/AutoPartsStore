6e33be92bca9210ffd6d07b67ca8b3b4a16f0d32
andriikuklinov <akuklinov@outlook.com>
2025-04-30 20:45:23 +0300

    Create request/response objects for GetCategories endpoint.

---
+++ AutoPartsStore/Category/Category.API/Models/GetCategories/GetCategoriesRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Category.API.Models.GetCategories
+{
+    public class GetCategoriesRequest
+    {
+        public string Filter { get; set; } = default!;
+        public string OrderBy { get; set; } = default!;
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+    }
+}
---
+++ AutoPartsStore/Category/Category.API/Models/GetCategories/GetCategoriesResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Category.API.Models.GetCategories
+{
+    public class GetCategoriesResponse
+    {
+        public IEnumerable<Category.API.Data.Entities.Category> Categories { get; private set; }
+
+        public GetCategoriesResponse(IEnumerable<Category.API.Data.Entities.Category> categories)
+        {
+            Categories = categories;
+        }
+    }
+}
