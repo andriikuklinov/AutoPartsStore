2131b8febf286905027f5376d04b6de1bff1e60e
andriikuklinov <akuklinov@outlook.com>
2025-04-30 20:46:42 +0300

    Add Get action method for CategoriesController.

---
+++ AutoPartsStore/Category/Category.API/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Category.API.Controllers
+{
+    [Route("/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly ISender _sender;
+
+        public CategoriesController(IMapper mapper, ISender sender)
+        {
+            _mapper = mapper;
+            _sender = sender;
+        }
+        [HttpGet]
+        public async Task<IResult> Get([FromQuery]GetCategoriesRequest request)
+        {
+            var query = _mapper.Map<GetCategoriesQuery>(request);
+            var result = await _sender.Send(query);
+            var response = _mapper.Map<GetCategoriesResponse>(result);
+
+            return Results.Ok(response);
+        }
+    }
+}
