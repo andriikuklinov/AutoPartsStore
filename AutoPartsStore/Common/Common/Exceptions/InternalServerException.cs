4ff3b5ecc5bea4c2c81e46443db224d2b8fef0cb
andriikuklinov <akuklinov@outlook.com>
2025-05-01 15:51:42 +0300

    Add custom InternalServerException class.

---
+++ AutoPartsStore/Common/Common/Exceptions/InternalServerException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Exceptions
+{
+    public class InternalServerException:Exception
+    {
+        public string Details { get; private set; }
+
+        public InternalServerException(string message):base(message)
+        {
+            
+        }
+        public InternalServerException(string message, string details):this(message)
+        {
+            Details = details;
+        }
+    }
+}
