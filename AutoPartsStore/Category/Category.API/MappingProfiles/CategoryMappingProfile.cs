dc9fcdc7a8e9ead81f8991db55cbb0d7c19708fa
andriikuklinov <akuklinov@outlook.com>
2025-05-04 00:45:07 +0300

    Add mapping schemas.

--- AutoPartsStore/Category/Category.API/MappingProfiles/CategoryMappingProfile.cs
+++ AutoPartsStore/Category/Category.API/MappingProfiles/CategoryMappingProfile.cs
@@ -1,7 +1,11 @@
 ﻿using Category.API.Categories.Commands.CreateCategory;
+using Category.API.Categories.Commands.DeleteCategory;
+using Category.API.Categories.Commands.UpdateCategory;
 using Category.API.Categories.Queries.GetAllCategories;
 using Category.API.Models.CreateCategory;
+using Category.API.Models.DeleteCategory;
 using Category.API.Models.GetAllCategories;
+using Category.API.Models.UpdateCategory;
 
 namespace Category.API.MappingProfiles
 {
@@ -17,6 +21,13 @@ namespace Category.API.MappingProfiles
 
             CreateMap<CreateCategoryRequest, CreateCategoryCommand>();
             CreateMap<CreateCategoryResult, CreateCategoryResponse>();
+
+            CreateMap<UpdateCategoryRequest, UpdateCategoryCommand>();
+            CreateMap<UpdateCategoryResult, UpdateCategoryResponse>();
+
+
+            CreateMap<DeleteCategoryRequest, DeleteCategoryCommand>();
+            CreateMap<DeleteCategoryResult, DeleteCategoryResponse>();
         }
     }
 }
